3d6b1b855834e5b06ba0f437e22353c8ed990d15
batuhankalayci <kalayi.batuhan@metu.edu.tr>
2024-02-19 15:58:53 +0300

    aaa

--- Index.R
+++ Index.R
@@ -16,20 +16,26 @@ mydice + 2
 now <- Sys.time()
 now
 
+install.packages(c("devtools", "roxygen2", "testthat", "rmarkdown", "pkgdown",
+                   "purrr"))
+usethis::use_git_config(user.name="batuhankalayci",
+                        user.email="kalayi.batuhan@metu.edu.tr")
+usethis::create_github_token()
 
+gitcreds::gitcreds_set()
 
+data(iris)
+head(iris)
+?iris
+class(iris$Sepal.Length)
+str(iris)
+summary(iris)
+iris$Sepal.Length
+iris[1,]
+iris[,1]
 
-
-
-
-
-
-
-
-
-
-
-
+mean(iris$Sepal.Length)
+sd(iris$Sepal.Length)
 
 
 
