98531c6ea345418a2122538e08d9d2e6bdfb6931
batuhankalayci <kalayi.batuhan@metu.edu.tr>
2024-02-19 15:58:03 +0300

    asaddaddaas

---
+++ Index.R
@@ -0,0 +1,65 @@
+# STAT 295
+# Revision
+
+a <- 1+1
+vec <- 1:100
+vec
+class(vec)
+typeof(vec)
+   
+1:6
+mydice <- 1:6
+mydice
+
+mydice + 2
+
+now <- Sys.time()
+now
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
